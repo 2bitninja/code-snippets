2b6a8bb610c25e26d56258a026cca9b88159e761
2bitninja <31809532+2bitninja@users.noreply.github.com>
2023-11-13 23:21:13 +0900

    Create example-yes-no-while.sh

---
+++ example-yes-no-while.sh
@@ -0,0 +1,9 @@
+function yes_or_no {
+    while true; do
+        read -p "$* [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;  
+            [Nn]*) echo "Aborted" ; return  1 ;;
+        esac
+    done
+}
