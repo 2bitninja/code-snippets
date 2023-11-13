e2734edbb675be881b453664f39b0eb75548c52e
2bitninja <31809532+2bitninja@users.noreply.github.com>
2023-11-13 23:18:51 +0900

    Create example-yes-no.sh

---
+++ example-yes-no.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Do you want to proceed? (yes/no) " yn
+
+case $yn in 
+	yes ) echo ok, we will proceed;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo invalid response;
+		exit 1;;
+esac
+
+echo doing stuff...
